69069a5da1dc209ac314b1d10b1a0d9594b0dfec
tobiaswes <tobias.west0@gmail.com>
2024-02-06 22:04:08 +0200

    Created new project

---
+++ src/main/java/calculator/compound_interest/CompoundInterestApplication.java
@@ -0,0 +1,13 @@
+package calculator.compound_interest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoundInterestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompoundInterestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/calculator/compound_interest/CompoundInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.compound_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompoundInterestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
